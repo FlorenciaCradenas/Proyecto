9466ca407bc2b5c0c9ddfda34d379ab42124a936
FlorenciaCradenas <zs16012138@estudiantes.uv.mx>
2020-05-29 18:18:47 -0500

    Se definio el mapa de las ciudades con sus vecinos

---
+++ main.py
@@ -0,0 +1,26 @@
+map = {
+    'A': ['N', 'S', 'M'],
+    'B': ['Z','X','J','V'],
+    'C': ['D','M','S'],
+    'D': ['C'],
+    'E': ['F','O'],
+    'F': ['E','H','Y'],
+    'G': ['T','P','X','Z'],
+    'H': ['F','X'],
+    'J': ['B','Y','O'],
+    'M': ['C','A'],
+    'N': ['O','A'],
+    'O': ['N','V','J','E'],
+    'P': ['T','G','U'],
+    'S': ['C','A','V'],
+    'T': ['P','G'],
+    'U': ['P','X'],
+    'V': ['B','O','S'],
+    'X': ['U','H','G','Z','B'],
+    'Y': ['F','J'],
+    'Z': ['G','X','B'],
+}
+# def amplitude(map, inicio, destino) ->	list:
+#     arbol(inicio)
+#     recorrido(inicio, None)
+#     while not Vacio(arbol)
